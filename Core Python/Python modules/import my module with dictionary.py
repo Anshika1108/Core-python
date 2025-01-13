b2bccc035cf8916fba3333fc7c935f0c3fed9425
Anshika1108 <anshikashrivansh@gmail.com>
2025-01-13 12:33:01 +0530

    hii

---
+++ Core Python/Python modules/import my module with dictionary.py
@@ -0,0 +1,9 @@
+import my_module_with_dictionary
+a = my_module_with_dictionary.person["name"]
+print(a)
+
+b = my_module_with_dictionary.person["age"]
+print(b)
+
+c = my_module_with_dictionary.person["place"]
+print(c)
---
+++ Core Python/Python modules/my_module_with_dictionary.py
@@ -0,0 +1,4 @@
+person = {"name":"Anshika",
+          "age":21,
+          "place":"indore"}
+
